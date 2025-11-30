fd16b6a8048490061d9dff5a03b8e57fe0901d6d
Rege-97 <cogus1027@gmail.com>
2025-12-01 02:01:44 +0900

    :wrench: chore: 전역 예외 처리 추가 (Validation 및 기본 예외 핸들링)

---
+++ src/main/java/com/rege/holiday/exception/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.rege.holiday.exception;
+
+import com.rege.holiday.common.response.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    // @RequestBody Validation 에러
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationException(MethodArgumentNotValidException ex) {
+        String msg = ex.getBindingResult().getFieldErrors().stream()
+                .map(FieldError::getDefaultMessage)
+                .collect(Collectors.joining(", "));
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ApiResponse.error(msg));
+    }
+
+    // 그 외 에러
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> handleOtherExceptions(Exception ex) {
+        log.error("Unhandled exception", ex);
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(ApiResponse.error("서버 내부 오류가 발생했습니다."));
+    }
+}
