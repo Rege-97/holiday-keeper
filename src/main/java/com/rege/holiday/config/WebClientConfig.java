b0b8f4edd35de87f87d3953a185e06fc9c377cb2
Rege-97 <cogus1027@gmail.com>
2025-12-01 05:17:16 +0900

    :sparkles: feat: text/json 응답을 위한 코덱 설정 추가

--- src/main/java/com/rege/holiday/config/WebClientConfig.java
+++ src/main/java/com/rege/holiday/config/WebClientConfig.java
@@ -1,9 +1,13 @@
 package com.rege.holiday.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.codec.json.Jackson2JsonDecoder;
+import org.springframework.http.codec.json.Jackson2JsonEncoder;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
@@ -12,10 +16,31 @@ public class WebClientConfig {
     private static final String BASE_URL = "https://date.nager.at/api/v3";
 
     @Bean
-    public WebClient webClient() {
+    public WebClient webClient(ObjectMapper objectMapper) {
+
+        ExchangeStrategies strategies = ExchangeStrategies.builder()
+                .codecs(configurer -> {
+                    Jackson2JsonDecoder decoder = new Jackson2JsonDecoder(
+                            objectMapper,
+                            MediaType.APPLICATION_JSON,
+                            new MediaType("text", "json")
+                    );
+
+                    Jackson2JsonEncoder encoder = new Jackson2JsonEncoder(
+                            objectMapper,
+                            MediaType.APPLICATION_JSON,
+                            new MediaType("text", "json")
+                    );
+
+                    configurer.defaultCodecs().jackson2JsonDecoder(decoder);
+                    configurer.defaultCodecs().jackson2JsonEncoder(encoder);
+                })
+                .build();
+
         return WebClient.builder()
                 .baseUrl(BASE_URL)
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .exchangeStrategies(strategies)
                 .build();
     }
 }
