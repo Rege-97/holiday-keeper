d0a554787d6f00b556e7be9008bfc2cabfbf3d6b
Rege-97 <cogus1027@gmail.com>
2025-12-01 01:44:31 +0900

    :wrench: chore: Swagger 설정 클래스 추가

---
+++ src/main/java/com/rege/holiday/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.rege.holiday.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Holiday Keeper API")
+                        .description("플랜잇스퀘어 백엔드 개발자 채용 과제 - 공휴일 관리 서비스")
+                        .version("v1.0.0"));
+    }
+}
